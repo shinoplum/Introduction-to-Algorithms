c82bb0232996a5aa492e7ecd74b044ab7d26b1e3
shinoship <shinoshipmail@gmail.com>
2025-09-28 21:01:58 +0800

    MAX-HEAPIFY and BUILD-MAX-HEAP

---
+++ sort_algorithms/HeapSort/MaxHeap.h
@@ -0,0 +1,68 @@
+//
+// Created by shinoship on 2025/9/28.
+//
+
+#ifndef ALGORITHMS_MAXHEAPIFY_H
+#define ALGORITHMS_MAXHEAPIFY_H
+#include<vector>
+using namespace std;
+
+//
+class MaxHeap {
+    public:
+    //MAX-HEAPIFY:维护最大堆性质
+    //递归实现
+    static void maxHeapify_rec(vector<int> &A, int i) {
+        int lchild = 2 * i + 1; //左孩子
+        int rchild = 2 * i + 2;//右孩子
+        int largest = i;//假设当前节点是最大值
+        int heap_size = A.size();
+
+        //比较左孩子
+        if (lchild < heap_size && A[lchild] > A[i]) {
+            largest = lchild;
+        }
+
+        //比较右孩子
+        if (rchild < heap_size && A[rchild] > A[largest]) {
+            largest = rchild;
+        }
+
+        //如果当前不是最大节点则下沉
+        if (largest != i) {
+            swap(A[i], A[largest]);
+            maxHeapify_rec(A, largest);
+        }
+    }
+
+    static void maxHeapify(vector<int>& A, int start) {
+        int i = start;
+        while (true) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int largest = i;
+            int heap_size = A.size();
+
+            if (left < heap_size && A[left] > A[largest])
+                largest = left;
+            if (right < heap_size && A[right] > A[largest])
+                largest = right;
+
+            if (largest == i) break;  // 堆性质已满足
+
+            swap(A[i], A[largest]);
+            i = largest;  // 继续下沉
+        }
+    }
+
+    //BUILD-MAX_HEAP:构建最大堆
+    static void buildMaxHeap(vector<int> &A) {
+        int heap_size = A.size();
+        for (int i = heap_size / 2 - 1; i >= 0; i--) {//子数组A([n/2] + 1...n)都是树的叶结点
+            maxHeapify_rec(A, i);
+        }
+    }
+};
+
+
+#endif //ALGORITHMS_MAXHEAPIFY_H
---
+++ sort_algorithms/HeapSort/maxheap_test.cpp
@@ -0,0 +1,42 @@
+//
+// Created by shinoship on 2025/9/28.
+//
+
+#include<vector>
+#include<iostream>
+#include"MaxHeap.h"
+using namespace std;
+
+int main() {
+    //maxHeapify_test
+    vector<int> A = {16, 4, 10, 14, 7, 9, 3, 2, 8, 1};  // 几乎是堆，但 A[1]=4 不满足
+    int heap_size = A.size();
+
+    cout << "Before MAX-HEAPIFY at index 1:\n";
+    for (int x : A) cout << x << " ";
+    cout << endl;
+
+    // 假设我们要修复以索引 1 为根的子树
+    MaxHeap::maxHeapify_rec(A,1);
+
+    cout << "After MAX-HEAPIFY:\n";
+    for (int x : A) cout << x << " ";
+    cout << endl;
+
+
+    //buildMaxHeap_test
+    vector<int> B = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+
+    cout << "Original array: ";
+    for (int x : B) cout << x << " ";
+    cout << endl;
+
+    MaxHeap::buildMaxHeap(B);
+
+    cout << "After buildMaxHeap: ";
+    for (int x : B) cout << x << " ";
+    cout << endl;
+
+
+    return 0;
+}
