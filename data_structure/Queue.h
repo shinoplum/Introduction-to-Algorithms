905c3ab86d714c5f3f79a92b3130bf312cdba366
shinoship <shinoshipmail@gmail.com>
2025-09-29 20:23:44 +0800

    stack and queue

--- data_structure/Queue.h
+++ data_structure/Queue.h
@@ -14,17 +14,33 @@ class Queue {
     int rear;
 };
 
-//
+//初始化
 void InitQueue(Queue &Q) {
-    Q.rear = Q.front = 0;//
+    Q.rear = Q.front = 0;
 }
 
-//
+//判队空
 bool isEmptyQueue(Queue Q) {
-    if (Q.rear == Q.front) {
+    if (Q.rear == Q.front) {//队空条件
         return true;
     }
     return false;
+}
+
+//入队
+bool EnQueue(Queue &Q, int x) {
+    if ((Q.rear + 1) % MAX_SIZE == Q.front) return false;
+    Q.data[Q.rear] = x;
+    Q.rear = (Q.rear + 1) % MAX_SIZE;
+    return true;
+}
+
+//出队
+bool DeQueue(Queue &Q, int &x) {
+    if (Q.rear == Q.front) return false;
+    x = Q.data[Q.front];
+    Q.front = (Q.front + 1) % MAX_SIZE;
+    return true;
 }
 
 #endif //ALGORITHMS_QUEUE_H
--- data_structure/Stack.h
+++ data_structure/Stack.h
@@ -13,25 +13,38 @@ class Stack {
     public:
     int data[MAX_SIZE];// 存储元素的数组
     int top;// 栈顶指针，-1 表示空栈
-
-    // 构造函数：初始化栈为空
-    Stack() : top(-1) {}
-
-    bool isEmpty() const {
-        return top == -1;
-    }
-
-    void push(int x) {
-        data[++top] = x;
-    }
-
-    int pop() {
-        if (isEmpty()) {
-            throw underflow_error("Stack is empty!");
-        }
-        int value = data[top--];
-        return value;
-    }
 };
+
+//初始化
+void InitStack(Stack &s) {
+    s.top = -1;
+}
+
+//判断栈空
+bool isEmptyStack(Stack s) {
+    if (s.top == -1) return true;
+    else return false;
+}
+
+//进栈
+bool Push(Stack &s, int x) {
+    if (s.top == MAX_SIZE - 1) return false;//栈满
+    s.data[++s.top] = x;
+    return true;
+}
+
+//出栈
+bool Pop(Stack &s) {
+    if (s.top == -1) return false;//栈空
+    x =s.data[top--];
+    return true;
+}
+
+//读栈顶元素
+bool getTop(Stack s, int &x) {
+    if (s.top == -1) return false;//栈空
+    x = s.data[s.top];
+    return true;
+}
 
 #endif //ALGORITHMS_STACK_H
